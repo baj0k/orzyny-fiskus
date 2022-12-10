0d4bc7bed00d11d9fbdba90f6ca1d7e535457426
aleksandra <aleksandra.borkowska14@gmail.com>
2022-12-10 19:28:06 +0100

    ocr first usage

--- orzyny_django/Orzyny/models.py
+++ orzyny_django/Orzyny/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Ocr(models.Model):
+    image = models.ImageField(upload_to='images/')
+
--- orzyny_django/Orzyny/views.py
+++ orzyny_django/Orzyny/views.py
@@ -2,11 +2,36 @@ from django.shortcuts import render
 from django.shortcuts import render
 from django.http import HttpRequest, Http404, HttpResponse
 from django.views import View
+import pytesseract
+from .forms import ImageUpload
+import os
+from PIL import Image
+from django.conf import settings
+
 
 # Create your views here.
+def index(request):
+    text = ""
+    message = ""
+    if request.method == 'POST':
+        form = ImageUpload(request.POST, request.FILES)
+        if form.is_valid():
+            # try:
+            form.save()
+            image = request.FILES['image']
+            image = image.name
+            path = settings.MEDIA_ROOT
+            pathz = path + "/images/" + image
 
-def button(request):
-    if request.method == "GET":
-        return render(request, "button.html")
-    if request.method == "POST":
-        return HttpResponse('<html><body>Miejsce dodane</body></html>')
+            text = pytesseract.image_to_string(Image.open(pathz))
+            text = text.encode("ascii", "ignore")
+            text = text.decode().split(' ')
+            os.remove(pathz)
+            # except:
+            #     message = "check your filename and ensure it doesn't have any space or check if it has any text"
+
+    context = {
+        'text': text,
+        'message': message,
+    }
+    return render(request, 'formpage.html', context)
